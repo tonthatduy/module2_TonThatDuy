7b62096dcc410e569a3d0236c62fe03a047b4f71
Béo Dui <tonthatduy1997@gmail.com>
2025-04-18 17:23:55 +0700

    bai tap

---
+++ module2/src/bai_tap_lam_them/quan_ly_khu_nghi_duong_furama/view/Main.java
@@ -0,0 +1,9 @@
+package bai_tap_lam_them.quan_ly_khu_nghi_duong_furama.view;
+
+import bai_tap_lam_them.quan_ly_khu_nghi_duong_furama.controllers.FuramaController;
+
+public class Main {
+    public static void main(String[] args) {
+        FuramaController.displayMainMenu();
+    }
+}
--- module2/src/ss10_dsa_danh_sach/bai_tap/bai_tap_2/quan_ly_phuong_tien_giao_thong/controller/VehicleController.java
+++ module2/src/ss10_dsa_danh_sach/bai_tap/bai_tap_2/quan_ly_phuong_tien_giao_thong/controller/VehicleController.java
@@ -25,7 +25,6 @@ public class VehicleController {
 
     public static void displayMenu() {
 
-
         menuLoop:
         do {
             System.out.println("--- VEHICLE MANAGEMENT PROGRAM ---");
